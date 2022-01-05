c01543e6b72089acf74de7e1cf7072000abbd229
Miika Nissi <miika@miikanissi.com>
2022-01-05 17:41:44 -0500

    [ADD] Added part1.11

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,5 +1,54 @@
 import {useState} from "react";
 
+function StatisticLine(props) {
+  return (
+    <tr>
+      <td>{props.text}</td>
+      <td>{props.value}</td>
+    </tr>
+  );
+}
+
+function Statistics(props) {
+  let statistics;
+  let all = props.good + props.neutral + props.bad;
+  let avg = (props.good - props.bad) / all;
+  let pos = (props.good / all) * 100;
+  if (all > 0) {
+    statistics = (
+      <div className="results">
+        <table>
+          <tbody>
+            <StatisticLine text="good" value={props.good} />
+            <StatisticLine text="neutral" value={props.neutral} />
+            <StatisticLine text="bad" value={props.bad} />
+            <StatisticLine text="all" value={all} />
+            <StatisticLine text="average" value={avg} />
+            <StatisticLine text="positive" value={pos + " %"} />
+          </tbody>
+        </table>
+      </div>
+    );
+  } else {
+    statistics = (
+      <div className="results">
+        <p>No feedback given</p>
+      </div>
+    );
+  }
+  return (
+    <div className="statistics">
+      <h1>statistics</h1>
+      {statistics}
+    </div>
+  );
+}
+
+function Button(props) {
+  const increaseButton = () => props.setter(props.counter + 1);
+  return <button onClick={increaseButton}>{props.text}</button>;
+}
+
 function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
@@ -9,19 +58,12 @@ function App() {
       <div className="feedback">
         <h1>give feedback</h1>
         <div className="buttons">
-          <button onClick={() => setGood(good + 1)}>good</button>
-          <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-          <button onClick={() => setBad(bad + 1)}>bad</button>
+          <Button counter={good} setter={setGood} text="good" />
+          <Button counter={neutral} setter={setNeutral} text="neutral" />
+          <Button counter={bad} setter={setBad} text="bad" />
         </div>
       </div>
-      <div className="statistics">
-        <h1>statistics</h1>
-        <div className="results">
-          <p>good {good}</p>
-          <p>neutral {neutral}</p>
-          <p>bad {bad}</p>
-        </div>
-      </div>
+      <Statistics good={good} bad={bad} neutral={neutral} />
     </div>
   );
 }
