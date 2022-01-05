f3e0ab199d4d56d603dc51acfb62b573356eee1f
Miika Nissi <miika@miikanissi.com>
2022-01-04 22:11:40 -0500

    [ADD] Part1.1 assignment, gitignore, readme update

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,50 @@
+function Header(props) {
+  return <h1>{props.course}</h1>;
+}
+
+function Content(props) {
+  return (
+    <div>
+      <p>
+        {props.part1} {props.exercises1}
+      </p>
+      <p>
+        {props.part2} {props.exercises2}
+      </p>
+      <p>
+        {props.part3} {props.exercises3}
+      </p>
+    </div>
+  );
+}
+
+function Total(props) {
+  return (
+    <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+  );
+}
+function App() {
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
+  return (
+    <div>
+      <Header course={course} />
+      <Content
+        part1={part1}
+        exercises1={exercises1}
+        part2={part2}
+        exercises2={exercises2}
+        part3={part3}
+        exercises3={exercises3}
+      />
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+    </div>
+  );
+}
+
+export default App;
