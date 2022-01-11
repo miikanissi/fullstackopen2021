1bc0bd938a1bc9303bf85236d03f2878b9f886be
Miika Nissi <miika@miikanissi.com>
2022-01-10 19:17:32 -0500

    [ADD] part2.4

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,5 +1,5 @@
 function Header({name}) {
-  return <h1>{name}</h1>;
+  return <h2>{name}</h2>;
 }
 
 function Part(props) {
@@ -25,38 +25,62 @@ function Total({parts}) {
   return <strong>Number of exercises: {total}</strong>;
 }
 
-function Course({course}) {
+function Course({courses}) {
   return (
     <div>
-      <Header name={course.name} />
-      <Content course={course} />
-      <Total parts={course.parts} />
+      <h1>Web development curriculum</h1>
+      {courses.map((course) => (
+        <div key={course.id}>
+          <Header name={course.name} />
+          <Content course={course} />
+          <Total parts={course.parts} />
+        </div>
+      ))}
     </div>
   );
 }
 
 function App() {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-        id: 3,
-      },
-    ],
-  };
-  return <Course course={course} />;
+  const courses = [
+    {
+      name: "Half Stack application development",
+      id: 1,
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: "State of a component",
+          exercises: 14,
+          id: 3,
+        },
+      ],
+    },
+    {
+      name: "Node.js",
+      id: 2,
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: "Middlewares",
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ];
+  return <Course courses={courses} />;
 }
 
 export default App;
