25c918fc19b753837a7f9535e8179a3ea66ad5e2
Miika Nissi <miika@miikanissi.com>
2022-01-30 14:47:15 -0500

    [ADD] Exercises 3.1. - 3.8.

---
+++ part3/phonebook/index.js
@@ -0,0 +1,105 @@
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+
+app.use(express.json());
+
+morgan.token("post", (request, _response) => {
+  "use strict";
+  if (request.method === "POST") return JSON.stringify(request.body);
+  else return "";
+});
+
+morgan.format(
+  "postFormat",
+  ":method :url :status :res[content-length] - :response-time ms | :post"
+);
+
+app.use(morgan("postFormat"));
+
+let persons = [
+  {
+    name: "Arto Hellas",
+    number: "040-123456",
+    id: 1,
+  },
+  {
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+    id: 2,
+  },
+  {
+    name: "Dan Abramov",
+    number: "12-43-234345",
+    id: 3,
+  },
+  {
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+    id: 4,
+  },
+];
+
+app.get("/info", (_request, response) => {
+  "use strict";
+  response.send(`
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${new Date()}</p>
+    `);
+});
+
+app.get("/api/persons", (_request, response) => {
+  "use strict";
+  response.json(persons);
+});
+
+app.post("/api/persons", (request, response) => {
+  "use strict";
+  const body = request.body;
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "name or number is missing",
+    });
+  } else if (persons.find((person) => person.name === body.name)) {
+    response.status(400).json({
+      error: "name must be unique",
+    });
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: Math.floor(Math.random() * 9999),
+  };
+
+  persons = persons.concat(person);
+
+  response.json(person);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  "use strict";
+  const id = Number(request.params.id);
+  const person = persons.find((per) => per.id === id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  "use strict";
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  "use strict";
+  console.log(`Server running on ${PORT}`);
+});
