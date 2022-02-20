1fa5e3325e910e9a91a49118aa4b278783970fe6
Miika Nissi <miika@miikanissi.com>
2022-02-20 14:35:40 -0500

    [ADD] forked patientor-frontend and fixed errors

--- part9/misc/README.md
+++ part9/misc/README.md
@@ -1,3 +1,4 @@
 # Miscellaneous Exercises
 
 This application contains miscellaneous TypeScript Exercises in parts 9.1.-9.7.
+These exercises include a BMI calculator and an exercise calculator with APIs.
---
+++ part9/patientor-frontend/src/App.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import axios from "axios";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import { Button, Divider, Header, Container } from "semantic-ui-react";
+
+import { apiBaseUrl } from "./constants";
+import { useStateValue } from "./state";
+import { Patient } from "./types";
+
+import PatientListPage from "./PatientListPage";
+
+const App = () => {
+  const [, dispatch] = useStateValue();
+  React.useEffect(() => {
+    void axios.get<void>(`${apiBaseUrl}/ping`);
+
+    const fetchPatientList = async () => {
+      try {
+        const { data: patientListFromApi } = await axios.get<Patient[]>(
+          `${apiBaseUrl}/patients`
+        );
+        dispatch({ type: "SET_PATIENT_LIST", payload: patientListFromApi });
+      } catch (e) {
+        console.error(e);
+      }
+    };
+    void fetchPatientList();
+  }, [dispatch]);
+
+  return (
+    <div className="App">
+      <Router>
+        <Container>
+          <Header as="h1">Patientor</Header>
+          <Button as={Link} to="/" primary>
+            Home
+          </Button>
+          <Divider hidden />
+          <Switch>
+            <Route path="/">
+              <PatientListPage />
+            </Route>
+          </Switch>
+        </Container>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
